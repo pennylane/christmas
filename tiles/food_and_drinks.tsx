753d06c1f9973eac7c1045a9e24237fe3028a3f7
pennylane <pennylane@pennylane.name>
2020-12-06 19:57:52 +0100

    Removed broken tileset

--- tiles/food_and_drinks.tsx
+++ tiles/food_and_drinks.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="2020.10.30" name="food_and_drinks" tilewidth="32" tileheight="32" tilecount="1024" columns="16">
- <image source="../../tiles/rc3_tiles/imgs/tilesheets/food_and_drinks.png" trans="ff00ff" width="512" height="2048"/>
+ <image source="food_and_drinks.png" trans="ff00ff" width="512" height="2048"/>
 </tileset>
