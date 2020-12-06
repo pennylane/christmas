996186948870a47e1ace6b30041d7842877d17b1
pennylane <pennylane@pennylane.name>
2020-12-06 15:20:33 +0000

    Deploying to gh-pages from master @ fdd58795b94bf7d34f9a88212ecd9c2c7299cf1e 🚀

--- tiles/cert_safety.tsx
+++ tiles/cert_safety.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.4" tiledversion="2020.10.30" name="cert_safety" tilewidth="32" tileheight="32" tilecount="1024" columns="16">
- <image source="../../tiles/rc3_tiles/imgs/tilesheets/cert_safety.png" trans="ff00ff" width="512" height="2048"/>
+ <image source="cert_safety.png" trans="ff00ff" width="512" height="2048"/>
 </tileset>
