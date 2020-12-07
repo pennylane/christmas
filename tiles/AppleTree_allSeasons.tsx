ad1697746823cb1ca8dcbfb5e615c84ee419afd7
pennylane <pennylane@pennylane.name>
2020-12-07 19:03:02 +0100

    Added winter backyard

---
+++ tiles/AppleTree_allSeasons.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.10.30" name="AppleTree_allSeasons" tilewidth="32" tileheight="32" tilecount="180" columns="15">
+ <image source="AppleTree_allSeasons.png" trans="ff00ff" width="480" height="384"/>
+</tileset>
---
+++ tiles/trees2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.10.30" name="trees2" tilewidth="32" tileheight="32" tilecount="196" columns="14">
+ <image source="trees2.png" trans="ff00ff" width="448" height="448"/>
+</tileset>
