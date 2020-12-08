79a2364f1b2926c1e4d03693fd3c1ee049643949
pennylane <pennylane@pennylane.name>
2020-12-08 22:55:16 +0100

    Added carpet and logos

---
+++ mdb.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2020.10.30" name="mdb" tilewidth="32" tileheight="32" tilecount="14" columns="7">
+ <image source="tiles/mdb.png" trans="ff00ff" width="224" height="64"/>
+</tileset>
