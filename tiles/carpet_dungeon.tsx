8aeab9a15094471e5318cb58bc584e74dc27b732
pennylane <pennylane@pennylane.name>
2020-12-09 10:04:54 +0000

    Deploying to gh-pages from master @ b5af99f4d0be6dafbd2c86c248a52eb88608fc16 🚀

--- tiles/carpet_dungeon.tsx
+++ tiles/carpet_dungeon.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="2020.10.30" name="Room1Tile" tilewidth="32" tileheight="32" tilecount="60" columns="5">
- <image source="Room1Tile.png" trans="b8b8f8" width="160" height="384"/>
+<tileset version="1.4" tiledversion="2020.10.30" name="carpet_dungeon" tilewidth="32" tileheight="32" tilecount="60" columns="5">
+ <image source="carpet_dungeon.png" width="160" height="384"/>
 </tileset>
